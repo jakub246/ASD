b844a135e8d089daf458362a67f56fbe4bc86576
jakub <sjakub@outlook.com>
2018-03-28 10:53:51 +0200

    first

---
+++ phoneBook/cmake-build-debug/CMakeFiles/phoneBook.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phoneBook.dir/main.cpp.o"
+  "phoneBook.pdb"
+  "phoneBook"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phoneBook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ phoneBook/cmake-build-debug/CMakeFiles/phoneBook.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for phoneBook.
+# This may be replaced when dependencies are built.
